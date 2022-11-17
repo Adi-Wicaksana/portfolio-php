a05b654906a54322f0ac662454156cf24d77156b
I Ketut Adi Wicaksana <adiwicaksana846@gmail.com>
2022-11-17 10:01:22 +0700

    Add works page

--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -1,77 +1,174 @@
 <div class="row row-no-margin swiper-slide">
     <div class="col-12 work-content-wrap">
-        <div class="container">
+        {{-- lg Device --}}
+        <div class="container d-none d-md-block">
             <div class="row">
                 <div class="col-12">
-                    <p style="font-family: 'Poppins-Bold'; font-size: 1.4rem"> Where I've Worked </p>
+                    <p class="font-title-work mb-font-title-work"> Where I've Worked </p>
                 </div>
             </div>
             <div class="row">
                 <div class="d-flex align-items-start">
-                    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical"
-                        style="border-left: 2px solid #233554; min-width: 215px;">
-                        <button class="btn btn-default" style="font-family: 'Poppins-Medium'; font-size: 1rem"
-                            id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button"
-                            role="tab" aria-controls="v-pills-home" aria-selected="true">PT. Akur Pratama</button>
+                    <div class="nav flex-column nav-pills company-list-div" id="v-pills-tab" role="tablist"
+                        aria-orientation="vertical">
+                        <button class="btn btn-default font-company-list" id="v-pills-home-tab" data-bs-toggle="pill"
+                            data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home"
+                            aria-selected="true">PT. Akur Pratama</button>
                     </div>
                     <div class="tab-content" id="v-pills-tabContent">
                         <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel"
                             aria-labelledby="v-pills-home-tab">
-                            <p style="font-family: 'Poppins-Medium'">Software Developer <br />
-                                <font style="font-size: 0.8rem">October 2018 - December 2020 (Yogya Online Department)
+                            <p class="font-job-title mb-sm-1 mb-lg-3">Software Developer <br />
+                                <font class="font-job-range">October 2018 - December 2020 (Yogya Online Department)
                                 </font>
                             </p>
 
                             <div>
-                                <p style="font-family: 'Poppins-Regular'; font-size: 0.8rem">
-                                    - Developed and maintained code for internal and external company apps,
-                                    primarly using HTML, CSS, JavaScript, jQuery, Laravel, PostgreSQL, Bootstrap, and
-                                    Patternfly.
-                                </p>
+                                <ul class="ps-2 ps-lg-4">
+                                    <li class="font-job-desc">
+                                        Developed and maintained code for internal and external company apps,
+                                        primarly using HTML, CSS, JavaScript, jQuery, Laravel, PostgreSQL, Bootstrap,
+                                        and Patternfly.
+                                    </li>
+                                    <li class="font-job-desc">
+                                        Manually tested internal and external apps in various browsers and mobile
+                                        devices
+                                        to ensure compatibility and responsiveness.
+                                    </li>
+                                    <li class="font-job-desc">
+                                        Add features to product information management, that changing the conventional
+                                        way
+                                        to the modern way.
+                                    </li>
+                                    <li class="font-job-desc">
+                                        Created order management system, that used by department to manage consumer
+                                        orders
+                                        from entering to the hands of consumers.
+                                    </li>
+                                </ul>
+                            </div>
 
-                                <p style="font-family: 'Poppins-Regular'; font-size: 0.8rem">
-                                    - Manually tested internal and external apps in various browsers and mobile devices
+                            <p class="font-job-title mb-sm-1 mb-lg-3">
+                                &nbsp;
+                                <font class="font-job-range mb-0-point-5">January 2021 - October 2021 (IT Solution &
+                                    Innovation)
+                                </font>
+                            </p>
+
+                            <div>
+                                <ul class="ps-2 ps-lg-4">
+                                    <li class="font-job-desc">
+                                        Collaborated with multivendor to develop mobile app called Yogya Online Mobile.
+                                        I'm on the backend, using sails.js, redis and creating a content management
+                                        system.
+                                    </li>
+                                    <li class="font-job-desc">
+                                        Be a leader for student interns.
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <p class="font-job-title mb-sm-1 mb-lg-3">
+                                &nbsp;
+                                <font class="font-job-range">October 2021 - Present (App Research & Development)
+                                </font>
+                            </p>
+
+                            <div>
+                                <ul class="ps-2 ps-lg-4">
+                                    <li class="font-job-desc">
+                                        Collaborated with teams to develop mobile app called mTransport - decentralized
+                                        delivery from dc to customer. I'm on the frontend, using react native.
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- sm Device --}}
+        <div class="container d-sm-block d-md-none">
+            <div class="row">
+                <div class="col-12">
+                    <p class="font-title-work mb-font-title-work"> Where I've Worked </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="btn btn-default font-company-list" id="pills-home-tab" data-bs-toggle="pill"
+                            data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
+                            aria-selected="true">PT. Akur Pratama</button>
+                    </li>
+                </div>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                        aria-labelledby="pills-home-tab">
+                        <p class="font-job-title mb-sm-1 mb-lg-3">Software Developer <br />
+                            <font class="font-job-range">October 2018 - December 2020 (Yogya Online Department)
+                            </font>
+                        </p>
+
+                        <div>
+                            <ul class="ps-2 ps-lg-4">
+                                <li class="font-job-desc">
+                                    Developed and maintained code for internal and external company apps,
+                                    primarly using HTML, CSS, JavaScript, jQuery, Laravel, PostgreSQL, Bootstrap,
+                                    and Patternfly.
+                                </li>
+                                <li class="font-job-desc">
+                                    Manually tested internal and external apps in various browsers and mobile
+                                    devices
                                     to ensure compatibility and responsiveness.
-                                </p>
-
-                                <p style="font-family: 'Poppins-Regular'; font-size: 0.8rem">
-                                    - Add features to product information management, that changing the conventional way
+                                </li>
+                                <li class="font-job-desc">
+                                    Add features to product information management, that changing the conventional
+                                    way
                                     to the modern way.
-                                </p>
-
-                                <p style="font-family: 'Poppins-Regular'; font-size: 0.8rem">
-                                    - Created order management system, that used by department to manage consumer orders
+                                </li>
+                                <li class="font-job-desc">
+                                    Created order management system, that used by department to manage consumer
+                                    orders
                                     from entering to the hands of consumers.
-                                </p>
-                            </div>
+                                </li>
+                            </ul>
+                        </div>
 
-                            <p style="font-family: 'Poppins-Medium'">
-                                <font style="font-size: 0.8rem">January 2021 - October 2021 (IT Solution & Innovation)
-                                </font>
-                            </p>
+                        <p class="font-job-title mb-sm-1 mb-lg-3">
+                            &nbsp;
+                            <font class="font-job-range mb-0-point-5">January 2021 - October 2021 (IT Solution &
+                                Innovation)
+                            </font>
+                        </p>
 
-                            <div>
-                                <p style="font-family: 'Poppins-Regular'; font-size: 0.8rem">
-                                    - Collaborated with multivendor to develop mobile app called Yogya Online Mobile.
-                                    I'm on the backend, using sails.js, redis and creating a content management system.
-                                </p>
+                        <div>
+                            <ul class="ps-2 ps-lg-4">
+                                <li class="font-job-desc">
+                                    Collaborated with multivendor to develop mobile app called Yogya Online Mobile.
+                                    I'm on the backend, using sails.js, redis and creating a content management
+                                    system.
+                                </li>
+                                <li class="font-job-desc">
+                                    Be a leader for student interns.
+                                </li>
+                            </ul>
+                        </div>
 
-                                <p style="font-family: 'Poppins-Regular'; font-size: 0.8rem">
-                                    - Be a leader for student interns.
-                                </p>
-                            </div>
+                        <p class="font-job-title mb-sm-1 mb-lg-3">
+                            &nbsp;
+                            <font class="font-job-range">October 2021 - Present (App Research & Development)
+                            </font>
+                        </p>
 
-                            <p style="font-family: 'Poppins-Medium'">
-                                <font style="font-size: 0.8rem">October 2021 - Present (App Research & Development)
-                                </font>
-                            </p>
-
-                            <div>
-                                <p style="font-family: 'Poppins-Regular'; font-size: 0.8rem">
-                                    - Collaborated with teams to develop mobile app called mTransport - decentralized
+                        <div>
+                            <ul class="ps-2 ps-lg-4">
+                                <li class="font-job-desc">
+                                    Collaborated with teams to develop mobile app called mTransport - decentralized
                                     delivery from dc to customer. I'm on the frontend, using react native.
-                                </p>
-                            </div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
